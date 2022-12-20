576ac75fe012dde8babd504b2be13d56feda996a
esdreyvg <Esdreyvg.softsytech@gmail.com>
2022-12-20 17:24:20 -0400

    .

--- shopping/src/App.js
+++ shopping/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Tienda E-commerce de Ropa
         </p>
         <a
           className="App-link"
