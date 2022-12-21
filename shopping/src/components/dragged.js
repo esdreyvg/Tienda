ad939ea689c435939d59745ea74cc779791af1c6
esdreyvg <Esdreyvg.softsytech@gmail.com>
2022-12-20 21:52:59 -0400

    creando componentes reutilizables lista y dragn and dropn

---
+++ shopping/src/components/dragged.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function dragn() {
+  return (
+    <div>dragged</div>
+  )
+}
+
+function dropn() {
+    return(
+        <div></div>
+    )
+}
+
+export {
+    dragn,
+    dropn
+}
---
+++ shopping/src/components/lists.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function simpleList({dataList}) {
+  return (
+    <div>lists</div>
+  )
+}
+
+function carrouselList({dataList}) {
+    return(
+        <div></div>
+    )
+}
+
+function productList({dataList}) {
+    return(
+        <div></div>
+    )
+}
+
+export {
+    simpleList,
+    carrouselList,
+    productList
+}
